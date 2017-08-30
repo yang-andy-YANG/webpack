548d8ded729aa4fe476a8a20a2b9ad18562308b6
Yang <andy.yang05@sap.com>
2017-08-30 11:21:32 +0800

    less is as default css pre-processor

--- template/src/components/Hello.vue
+++ template/src/components/Hello.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="hello">
-    <h1>\{{ msg }}</h1>
-    <h2>Essential Links</h2>
+    <h1>{{ msg }}</h1>
+    <h2>Essential Links!!!!!</h2>
     <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
+      <li><a class="link clearfix" href="https://vuejs.org" target="_blank">Core Docs</a></li>
       <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
+      <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
       <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
       <br>
       <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
@@ -23,31 +23,20 @@
 <script>
 export default {
   name: 'hello',
-  data{{#unless_eq lintConfig "airbnb"}} {{/unless_eq}}() {
+  data () {
     return {
-      msg: 'Welcome to Your Vue.js App'{{#if_eq lintConfig "airbnb"}},{{/if_eq}}
-    }{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
-  }{{#if_eq lintConfig "airbnb"}},{{/if_eq}}
-}{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
+<style scoped lang="less">
+  @import '~@/style/link';
+  @import '~@/style/mixin';
+  .hello {
+    background-color: @bg-2;
+    .sc(20px)
+  }
 </style>
